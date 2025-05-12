462780fe21f270901d394e0ab7376e13036ea44d
Morteza Raeisi <mrraeisi@outlook.com>
2025-05-12 23:07:36 +0330

    impl EncryptedFile protocol

--- src/io.rs
+++ src/io.rs
@@ -1,5 +1,4 @@
-use crate::errors::MyResult;
-use crate::fs::*;
+use crate::{cipher::Cipher, errors::MyResult, fs::*};
 
 pub trait File {
     async fn write(&mut self, buf: &[u8]) -> MyResult;
@@ -28,6 +27,42 @@ impl File for ClearFile {
 
     async fn read(&self) -> MyResult<Vec<u8>> {
         read(&self.path).await
+    }
+
+    async fn delete(self) -> MyResult {
+        delete(&self.path).await
+    }
+}
+
+pub struct EncryptFile {
+    path: std::path::PathBuf,
+    cipher: Cipher,
+}
+impl EncryptFile {
+    pub fn new(path: std::path::PathBuf, cipher: Cipher) -> Self {
+        Self { path, cipher }
+    }
+}
+impl File for EncryptFile {
+    async fn write(&mut self, buf: &[u8]) -> MyResult {
+        let payload = self.cipher.encrypt(buf).map_err(|_| {
+            std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                "Invalid data for encryption",
+            )
+        })?;
+        write(&self.path, payload.as_slice()).await
+    }
+
+    async fn read(&self) -> MyResult<Vec<u8>> {
+        let payload = read(&self.path).await?;
+        let x = self.cipher.decrypt(payload.as_slice()).map_err(|_| {
+            std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                "Invalid data for decryption",
+            )
+        })?;
+        Ok(x)
     }
 
     async fn delete(self) -> MyResult {
