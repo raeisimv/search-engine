2608447ddc0e52bb2713ee907c24f52e757d5049
Morteza Raeisi <mrraeisi@outlook.com>
2025-05-14 20:21:05 +0330

    impl creating & browsing directory operations  - add futures-lite crate

--- src/fs/dir.rs
+++ src/fs/dir.rs
@@ -1,5 +1,6 @@
 use crate::{
     cipher::Cipher,
+    errors::MyResult,
     fs::io::{ClearFile, EncryptFile},
 };
 use std::path::{Path, PathBuf};
@@ -14,5 +15,24 @@ impl Directory {
     }
     pub fn make_encrypted_file(&self, relative: impl AsRef<Path>) -> EncryptFile {
         EncryptFile::new(self.path_buf.join(relative).into(), self.cipher.clone())
+    }
+
+    pub async fn create(&self) -> MyResult {
+        async_fs::create_dir_all(&self.path_buf).await?;
+        Ok(())
+    }
+
+    pub async fn get_files(&self) -> MyResult<Vec<PathBuf>> {
+        use futures_lite::stream::StreamExt;
+
+        let mut files = async_fs::read_dir(&self.path_buf).await?;
+        let mut paths = vec![];
+        while let Ok(Some(x)) = files.try_next().await {
+            let path = x.path();
+            if path.is_file() {
+                paths.push(path);
+            }
+        }
+        Ok(paths)
     }
 }
