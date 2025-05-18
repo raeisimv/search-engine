c02179b3bc43f992b6bee83acffaf54cc6508e96
Morteza Raeisi <mrraeisi@outlook.com>
2025-05-18 22:39:04 +0330

    impl conversion from PersistentCollection into a browsable collection

--- src/index.rs
+++ src/index.rs
@@ -8,7 +8,7 @@ pub type DocumentIdentifier = Arc<str>;
 pub struct Entry {
     pub index: EntryIndex,
     pub name: DocumentIdentifier,
-    pub sharding: ShardingValue,
+    pub shard: ShardingValue,
 }
 pub struct PersistedCollection {
     pub entries: Vec<Entry>,
@@ -17,4 +17,21 @@ pub struct Collection {
     entries_by_index: HashMap<EntryIndex, DocumentIdentifier>,
     entries_by_name: HashMap<DocumentIdentifier, EntryIndex>,
     sharding: BTreeMap<ShardingValue, HashSet<EntryIndex>>,
+}
+impl From<PersistedCollection> for Collection {
+    fn from(value: PersistedCollection) -> Self {
+        let mut entries_by_name = HashMap::with_capacity(value.entries.len());
+        let mut entries_by_index = HashMap::with_capacity(value.entries.len());
+        let mut sharding: BTreeMap<ShardingValue, HashSet<EntryIndex>> = BTreeMap::new();
+        for x in value.entries {
+            entries_by_index.insert(x.index, x.name.clone());
+            entries_by_name.insert(x.name.clone(), x.index);
+            sharding.entry(x.shard).or_default().insert(x.index);
+        }
+        Collection {
+            entries_by_index,
+            entries_by_name,
+            sharding,
+        }
+    }
 }
